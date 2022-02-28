9a754201e0766c886a9c687bfda06e8dcf8caea9
Angel <al221910938@gmail.com>
2022-02-28 16:56:08 -0600

    usuarios api

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\UserCollection;
+use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -36,7 +37,18 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'Nombre' => 'required|string|max:25',
+            'Apellido_Paterno' => 'required|string|max:25',
+            'Apellido_Materno' => 'required|nullable|string|max:25',
+            'Fecha_Nacimiento' => 'required|date_format:Y-m-d',
+            'Tipo_Usuario' => 'required|in:Administrador, Usuario, Usuario_Privilegiado',
+            'Email' => 'required|email|unique:users,email',
+            'Password' => 'required|min:8'
+        ]);
+        $usuario = User::create($request->all());
+        // return new UserResource($usuario);
+        return ['success' => "Usuario Creado Correctamente"];
     }
 
     /**
@@ -47,7 +59,8 @@ class UserController extends Controller
      */
     public function show($id)
     {
-        //
+        $user = User::find($id);
+        return $user;
     }
 
     /**
@@ -70,7 +83,31 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'Nombre' => 'required|string|max:25',
+            'Apellido_Paterno' => 'required|string|max:25',
+            'Apellido_Materno' => 'bail|nullable|string|max:25',
+            'Fecha_Nacimiento' => 'required|date_format:Y-m-d',
+            'Tipo_Usuario' => 'required|in:Administrador, Usuario, Usuario_Privilegiado',
+            'Email' => 'required|email',
+            'Password' => 'required|min:8'
+        ]);
+
+        $user = User::find($id);
+
+        $user->Nombre = $request->Nombre;
+        $user->Apellido_Paterno = $request->Apellido_Paterno;
+        $user->Apellido_Materno = $request->Apellido_Materno;
+        $user->Fecha_Nacimiento = $request->Fecha_Nacimiento;
+        $user->Tipo_Usuario = $request->Tipo_Usuario;
+        $user->Email = $request->Email;
+        $user->Password = $request->Password;
+
+        $user->save();
+
+        // se necesita validar por algun error en el correo
+
+        return ['success' => "Usuario Actualizado Correctamente"];
     }
 
     /**
@@ -81,6 +118,8 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $user = User::find($id);
+        $user->delete();
+        return ['success' => "Usuario Eliminado Correctamente"];
     }
 }
