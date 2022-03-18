62cc620a45b59280e732bf13059bbb059f694bda
Angel <al221910938@gmail.com>
2022-03-17 20:48:41 -0600

    agregar tabla sub servicios agregar campo foto

--- database/migrations/2022_02_28_214413_create_negocios_table.php
+++ database/migrations/2022_02_28_214413_create_negocios_table.php
@@ -20,6 +20,7 @@ class CreateNegociosTable extends Migration
             $table->string('Horario_Servicio', 25)->comment('Horario_Servicio');
             $table->string('Dias_Servicio', 25)->comment('Dias_Servicio');
             $table->string('Descripcion_Del_Negocio', 100)->comment('Descripcion_Del_Negocio');
+            $table->string('Foto', 25)->comment('Foto');
             $table->timestamps();
 
             $table->unsignedBigInteger('Usuario_Id');
